0362e6353187a465f7b9c7f59114c0d055854f6b
Pherrows <yhk1492@naver.com>
2023-07-13 18:46:48 +0900

    20 23 7 13
    집

--- 10_React/my-shop/src/api/productAPI.js
+++ 10_React/my-shop/src/api/productAPI.js
@@ -6,54 +6,61 @@ import axios from "axios";
 // 상품 목록 조회
 export const getProducts = async () => {
   try {
-    const response = await axios.get('https://my-json-server.typicode.com/Pherrows/db-shop/products')
-    
-    if (response.status === 200) { // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
-      return response.data
-    } else {
-      throw new Error(`api error: ${response.status} ${response.statusText}`)
-    }
+    const response = await axios.get(
+      "https://my-json-server.typicode.com/Pherrows/db-shop/products"
+    );
 
-  } catch (error) { // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
+    if (response.status === 200) {
+      // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
+      return response.data;
+    } else {
+      throw new Error(`api error: ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
     console.error(error);
     throw error;
   }
-  
 };
 
 // 특정 상품 조회
 
 export const getProductsById = async (id) => {
   try {
-    const response = await axios.get(`https://my-json-server.typicode.com/Pherrows/db-shop/products/${id}`)
+    const response = await axios.get(
+      `https://my-json-server.typicode.com/Pherrows/db-shop/products/${id}`
+    );
 
-    if (response.status === 200) { // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
-      return response.data
+    if (response.status === 200) {
+      // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
+      return response.data;
     } else {
-      throw new Error(`api error: ${response.status} ${response.statusText}`)
+      throw new Error(`api error: ${response.status} ${response.statusText}`);
     }
-
-  } catch (error) { // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
+  } catch (error) {
+    // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
     console.error(error);
     throw error;
   }
 };
-
 
 // 만약 서버로 데이터를 보내야 한다면?
 // json-server 이용 시 테스트 가능
 
 export const addProduct = async (product) => {
   try {
-    const response = await axios.post(`http://localhost:4000/products`, { product })
+    const response = await axios.post(`http://localhost:4000/products`, {
+      product,
+    });
 
-    if (response.status === 200) { // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
-      return response.data
+    if (response.status === 200) {
+      // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
+      return response.data;
     } else {
-      throw new Error(`api error: ${response.status} ${response.statusText}`)
+      throw new Error(`api error: ${response.status} ${response.statusText}`);
     }
-
-  } catch (error) { // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
+  } catch (error) {
+    // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
     console.error(error);
     throw error;
   }
@@ -61,15 +68,19 @@ export const addProduct = async (product) => {
 
 export const orderProduct = async (productId, orderCount) => {
   try {
-    const response = await axios.post(`http://localhost:4000/products-drder`, { productId, orderCount })
-    
-    if (response.status === 200) { // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
-      return response.data
-    } else {
-      throw new Error(`api error: ${response.status} ${response.statusText}`)
-    }
+    const response = await axios.post(`http://localhost:4000/products-drder`, {
+      productId,
+      orderCount,
+    });
 
-  } catch (error) { // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
+    if (response.status === 200) {
+      // 요청에 대한 응답의 상태가 200 ok 일떄만 결과를 리턴
+      return response.data;
+    } else {
+      throw new Error(`api error: ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    // 서버가 죽었거나,인터넷이 끊겼거나, URL이 잘못됐을 떄 등
     console.error(error);
     throw error;
   }
